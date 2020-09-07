39b271d06f31a3929525576e0a15fdc92154d6a3
umeshbhat <umeshbhat30@gmail.com>
2020-09-07 14:02:18 +0530

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -20,3 +20,4 @@ export * from './events/order-cancelled-event'
 export * from './events/order-created-event'
 
 export * from './events/types/order-status'
+export * from './events/expiration-complete-event'
