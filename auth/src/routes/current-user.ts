c3800940910c4c1edb4fe9d9304f23441376ff2c
umeshbhat <umeshbhat30@gmail.com>
2020-09-15 21:01:53 +0530

    umesh

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,8 @@
 import { currentUser } from '@umeshbhatorg/common'
 import express from 'express'
 const router = express.Router()
+
+
 router.get('/api/users/currentuser', currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null })
 })
